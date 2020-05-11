3cbf98deb51455633a008f8e00b7e65b92c373b4
Dave Berkeley <via@rotwang.co.uk>
2020-05-11 11:28:01 +0100

    porting to stmf103

--- stm32/gpio_arm.h
+++ stm32/gpio_arm.h
@@ -2,6 +2,13 @@
 #if !defined(__GPIO_ARM_H__)
 #define __GPIO_ARM_H__
 
+#if defined(STM32F1xx)
+#include "stm32f1xx_hal_gpio.h"
+#endif
+#if defined(STM32F4xx)
+#include "stm32f4xx_hal_gpio.h"
+#endif
+
 namespace panglos {
 
 GPIO * gpio_create(GPIO_TypeDef *_port, uint16_t _pin, uint32_t mode);
