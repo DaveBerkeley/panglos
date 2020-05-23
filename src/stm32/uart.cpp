4d91e550da13bd9601e24e0f87461442a2791536
Dave Berkeley <via@rotwang.co.uk>
2020-05-23 13:22:52 +0100

    reverted uart to remove dma/timer code

--- src/stm32/uart.cpp
+++ src/stm32/uart.cpp
@@ -3,10 +3,7 @@
 #include <string.h>
 #include <stdarg.h>
 
-#include "../panglos/stm32/stm32fxxx_hal.h"
 #include "../panglos/stm32/hal.h"
-
-#include <stm32f1xx_hal_dma.h>
 
 #include "../panglos/debug.h"
 
@@ -15,55 +12,7 @@
 #include "../panglos/list.h"
 #include "../panglos/uart.h"
 
-// TODO : remove me?
-#include "../panglos/dispatch.h"
-#include "../panglos/list.h"
-extern panglos::Dispatch dispatch;
-
 namespace panglos {
-
-    /*
-     *  DMA
-     */
-
-#define DISPATCH_BUFF_SIZE 16
-#define DISPATCH_BUFFERS 8
-
-class Callback : public Dispatch::Callback
-{
-    UART::Buffer *uart;
-public:
-    uint8_t buff[DISPATCH_BUFF_SIZE];
-    int in;
-
-    Callback(UART::Buffer *b) 
-    :   Dispatch::Callback(0), uart(b), in(0)
-    {
-        // TODO : this should be set by the DMA Rx code
-        in = sizeof(buff);
-    }
-
-    virtual void execute()
-    { 
-        ASSERT(uart);
-        uart->add(buff, in);
-        // return this callback to the pool
-        list.push(this, mutex);
-    }
-
-    typedef List<Callback*> Callbacks;
-    static Callbacks list;
-    static Callback **next_fn(Callback *cb) { return (Callback**) & cb->next; }
-    static Mutex *mutex;
-};
-
-Callback::Callbacks Callback::list(Callback::next_fn);
-
-Mutex *Callback::mutex = Mutex::create_critical_section();
-
-    /*
-     *
-     */
 
 static UART_HandleTypeDef uart1;
 static UART_HandleTypeDef uart2;
@@ -72,6 +21,10 @@ static UART_HandleTypeDef uart3;
     /*
      *
      */
+
+class ArmUart;
+
+static ArmUart *uarts[3];
 
 static UART_HandleTypeDef *get_uart(UART::Id id)
 {
@@ -86,190 +39,6 @@ static UART_HandleTypeDef *get_uart(UART::Id id)
     return 0;
 }
 
-    /*
-     *
-     */
-
-class ArmUart : public UART
-{
-public:
-    Id id;
-
-    UART_HandleTypeDef *handle;
-    Buffer *buffer;
-    uint32_t error;
-    Callback *next_cb;
-
-    virtual uint32_t get_error() { uint32_t e = error; error = 0; return e; }
-
-public:
-    ArmUart(Id _id, UART_HandleTypeDef *_handle, Buffer *b);
-
-    ~ArmUart();
-
-    // Implement Output class
-    virtual int _putc(char c);
-    virtual int _puts(char *s, int n);
-
-    // Implement UART class
-    virtual int send(const char* data, int n);
-
-    int on_rx(const uint8_t *data, int size);
-
-    void irq();
-
-    void set_error(uint32_t e) { error |= e; }
-
-    Callback *get_cb()
-    {
-        if (!next_cb)
-        {
-            next_cb = Callback::list.pop(Callback::mutex);
-        }
-
-        return next_cb;
-    }
-};
-
-static ArmUart *uarts[3];
-
-    /*
-     *
-     */
-
-static DMA_HandleTypeDef dma_rx_1;
-static DMA_HandleTypeDef dma_tx_1;
-static DMA_HandleTypeDef dma_rx_2;
-static DMA_HandleTypeDef dma_tx_2;
-static DMA_HandleTypeDef dma_rx_3;
-static DMA_HandleTypeDef dma_tx_3;
-
-#if defined(STM32F1xx)
-
-static DMA_Channel_TypeDef *get_dma_instance(UART::Id id, bool rx)
-{
-    // See manual section 13.3.7 on DMA request mapping
-    switch (id)
-    {
-        case UART::UART_1 : return rx ? DMA1_Channel5 : DMA1_Channel4;
-        case UART::UART_2 : return rx ? DMA1_Channel6 : DMA1_Channel7;
-        case UART::UART_3 : return rx ? DMA1_Channel3 : DMA1_Channel2;
-        default : ASSERT(0);
-    }
-    return 0;
-}
-
-static IRQn_Type get_dma_irq(UART::Id id, bool rx)
-{
-    switch (id)
-    {
-        case UART::UART_1 : return rx ? DMA1_Channel5_IRQn : DMA1_Channel4_IRQn;
-        case UART::UART_2 : return rx ? DMA1_Channel6_IRQn : DMA1_Channel7_IRQn;
-        case UART::UART_3 : return rx ? DMA1_Channel3_IRQn : DMA1_Channel2_IRQn;
-        default : ASSERT(0);
-    }
-    return (IRQn_Type) 0;
-}
-
-static DMA_HandleTypeDef *get_dma_handle(UART::Id id, bool rx)
-{
-    switch (id)
-    {
-        case UART::UART_1 : return rx ? & dma_rx_1 : & dma_tx_1;
-        case UART::UART_2 : return rx ? & dma_rx_2 : & dma_tx_2;
-        case UART::UART_3 : return rx ? & dma_rx_3 : & dma_tx_3;
-        default : ASSERT(0);
-    }
-    return 0;
-}
-
-#endif
-
-static void dma_init(UART::Id id, bool rx, int irq_level)
-{
-    // DMA Rx init
-    UART_HandleTypeDef *uart = get_uart(id);
-    DMA_HandleTypeDef *dma = get_dma_handle(id, rx);
-
-    // enable dma interface ck
-    __HAL_RCC_DMA1_CLK_ENABLE();
-
-    // configure handle with required rx/tx params
-    dma->Instance = get_dma_instance(id, rx);
-
-    dma->Init.Direction = rx ? DMA_PERIPH_TO_MEMORY : DMA_MEMORY_TO_PERIPH;
-    dma->Init.PeriphInc = DMA_PINC_DISABLE;
-    dma->Init.MemInc = DMA_MINC_ENABLE;
-    dma->Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-    dma->Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-    dma->Init.Mode = DMA_NORMAL; // or DMA_CIRCULAR
-    dma->Init.Priority = DMA_PRIORITY_HIGH;
-
-    // configure the dma tx/rx channel
-    HAL_StatusTypeDef status = HAL_DMA_Init(dma);
-    ASSERT(status == HAL_OK);
-
-    // associate dma handle with the tx/rx handle
-    if (rx)
-    {
-        __HAL_LINKDMA(uart, hdmarx, (*dma));
-    }
-    else
-    {
-        __HAL_LINKDMA(uart, hdmatx, (*dma));
-    }
-
-    // configure priority and enable NVIC for xfer complete irq
-    HAL_NVIC_SetPriority(get_dma_irq(id, rx), irq_level, 0);
-    HAL_NVIC_EnableIRQ(get_dma_irq(id, rx));
-}
-
-    /*
-     *
-     */
-
-static void dma_start_rx(ArmUart *uart, DMA_HandleTypeDef *dma)
-{
-    Callback *cb = uart->get_cb();
-    ASSERT(cb);
-    HAL_StatusTypeDef status = HAL_UART_Receive_DMA(uart->handle, cb->buff, sizeof(cb->buff));
-    ASSERT(status == HAL_OK);
-}
-
-    /*
-     *
-     */
-
-extern "C" void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
-{
-    ASSERT(huart == & uart1);
-
-    ArmUart *uart = uarts[0];
-
-    // TODO : how many chars have been xferred?
-
-    Callback *cb = uart->get_cb();
-    uart->next_cb = 0;
-
-    dma_start_rx(uart, & dma_rx_1);
-
-    if (cb)
-    {
-        dispatch.put(cb);
-    }
-}
-
-extern "C" void DMA1_Channel5_IRQHandler(void)
-{
-    HAL_DMA_IRQHandler(& dma_rx_1);
-}
- 
-extern "C" void DMA1_Channel4_IRQHandler(void)
-{
-    ASSERT(0);
-    HAL_DMA_IRQHandler(& dma_tx_1);
-}
- 
 static IRQn_Type get_irq_num(UART::Id id)
 {
     switch (id)
@@ -527,6 +296,8 @@ static UART_HandleTypeDef* MX_UART_Init(panglos::UART::Id id, uint32_t baud, int
     HAL_NVIC_SetPriority(irq_num, irq_level, 0);
     HAL_NVIC_EnableIRQ(irq_num);
 
+    __HAL_UART_ENABLE_IT(uart, UART_IT_RXNE);
+
     return uart;
 }
 
@@ -543,56 +314,70 @@ static void MX_UART_Deinit(panglos::UART::Id id)
      *
      */
 
-ArmUart::ArmUart(Id _id, UART_HandleTypeDef *_handle, Buffer *b)
-: id(_id), handle(_handle), buffer(b), error(0), next_cb(0)
-{   
-    ASSERT(buffer);
-    uarts[id] = this;
-}
-
-ArmUart::~ArmUart()
+class ArmUart : public UART
 {
-    MX_UART_Deinit(id);
-    uarts[id] = 0;
-}
+    Id id;
 
-// Implement Output class
-int ArmUart::_putc(char c)
-{
-    return send(& c, 1);
-}
+    UART_HandleTypeDef *handle;
+    RingBuffer<uint8_t> *buffer;
+    uint32_t error;
 
-int ArmUart::_puts(char *s, int n)
-{
-    return send(s, n);
-}
+    virtual uint32_t get_error() { uint32_t e = error; error = 0; return e; }
 
-int ArmUart::on_rx(const uint8_t *data, int size)
-{
-    return buffer->add(data, size);
-}
-
-void ArmUart::irq()
-{
-    ASSERT(buffer);
-
-    static const uint32_t err_mask = UART_FLAG_ORE | UART_FLAG_NE | UART_FLAG_FE | UART_FLAG_PE;
-    const uint32_t sr = handle->Instance->SR;
-
-    const uint32_t err = sr & err_mask;
-
-    if (err)
-    {
-        set_error(err);
-        __HAL_UART_CLEAR_FLAG(handle, err);
+public:
+    ArmUart(Id _id, UART_HandleTypeDef *_handle, RingBuffer<uint8_t> *b)
+    : id(_id), handle(_handle), buffer(b)
+    {   
+        ASSERT(buffer);
+        mutex = Mutex::create();
+        uarts[id] = this;
     }
 
-    if (sr & UART_FLAG_RXNE)
+    ~ArmUart()
     {
-        uint8_t data = handle->Instance->DR;
-        on_rx(& data, 1);
+        MX_UART_Deinit(id);
+        uarts[id] = 0;
     }
-}
+
+    // Implement Output class
+    virtual int _putc(char c)
+    {
+        return send(& c, 1);
+    }
+    virtual int _puts(char *s, int n)
+    {
+        return send(s, 1);
+    }
+
+    // Implement UART class
+    virtual int send(const char* data, int n);
+
+    void irq()
+    {
+        ASSERT(buffer);
+
+        static const uint32_t err_mask = UART_FLAG_ORE | UART_FLAG_NE | UART_FLAG_FE | UART_FLAG_PE;
+        const uint32_t sr = handle->Instance->SR;
+
+        const uint32_t err = sr & err_mask;
+
+        if (err)
+        {
+            set_error(err);
+            __HAL_UART_CLEAR_FLAG(handle, err);
+        }
+
+        if (sr & UART_FLAG_RXNE)
+        {
+            const uint8_t data = handle->Instance->DR;
+            /* Clear RXNE interrupt flag */
+            //__HAL_UART_SEND_REQ(handle, UART_RXDATA_FLUSH_REQUEST);
+            buffer->add(data);
+        }
+    }
+
+    void set_error(uint32_t e) { error |= e; }
+};
 
     /*
      *
@@ -608,7 +393,6 @@ int ArmUart::send(const char* data, int n)
     // TODO : where is this global mutex!
     Lock lock(mutex);
 
-    //HAL_UART_Transmit_DMA(handle, (uint8_t*) data, n);
     HAL_UART_Transmit(handle, (uint8_t*) data, n, 100);
     return n;
 }
@@ -620,36 +404,7 @@ int ArmUart::send(const char* data, int n)
 UART *UART::create(UART::Id id, int baud, Buffer *b, int irq_level)
 {
     UART_HandleTypeDef *uart = MX_UART_Init(id, baud, irq_level);
-    ArmUart * arm_uart = new ArmUart(id, uart, b);
-
-    if (id == UART::UART_1)
-    {
-        if (Callback::list.empty())
-        {
-            Lock lock(Callback::mutex);
-
-            for (int i = 0; i < DISPATCH_BUFFERS; i++)
-            {
-                Callback *cb = new Callback(arm_uart->buffer);
-                Callback::list.push(cb, 0);
-            }
-        }
-
-        dma_init(id, true, irq_level);
-        dma_start_rx(arm_uart, get_dma_handle(id, true));
-    }
-    else
-    {
-        // Use Rx Not Empty interrupt for the other UARTs
-        __HAL_UART_ENABLE_IT(uart, UART_IT_RXNE);
-    }
-
-    if (id == UART::UART_3)
-    {
-        //dma_init(id, false, irq_level);
-    }
-    
-    return arm_uart;
+    return new ArmUart(id, uart, b);
 }
 
     /*
@@ -675,15 +430,9 @@ static void uart_rx_irq(ArmUart *uart)
 
 using namespace panglos;
 
-extern "C" void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
-{
-    // TODO : called on error. Needs to recover from it, especially during DMA.
-    //ASSERT(0);
-}
-
 extern "C" void USART1_IRQHandler(void)
 {
-    HAL_UART_IRQHandler(& uart1);
+    uart_rx_irq(uarts[0]);
 }
 
 extern "C" void USART2_IRQHandler(void)
