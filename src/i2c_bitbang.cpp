670a76e39e5e567bb1cdd0125e353e92f18e1205
Dave Berkeley <via@rotwang.co.uk>
2022-06-18 12:00:17 +0100

    added verbose to i2c bitbang

--- src/i2c_bitbang.cpp
+++ src/i2c_bitbang.cpp
@@ -23,7 +23,7 @@ void BitBang_I2C::wait()
 
 void BitBang_I2C::start()
 {
-    //PO_DEBUG("");
+    if (verbose) PO_DEBUG("");
     scl->set(1);
     sda->set(0);
     wait();
@@ -33,12 +33,12 @@ void BitBang_I2C::start()
 
 bool BitBang_I2C::bit_io(bool bit)
 {
-    //PO_DEBUG("bit=%d", bit);
     sda->set(bit);
     wait();
     scl->set(1);
     wait();
     const bool d = sda->get();
+    if (verbose) PO_DEBUG("wr=%d rd=%d", bit, d);
     scl->set(0);
     wait();
     return d;
@@ -46,19 +46,20 @@ bool BitBang_I2C::bit_io(bool bit)
 
 bool BitBang_I2C::ack()
 {
-    //PO_DEBUG("");
+    if (verbose) PO_DEBUG("");
     // Handle clock stretching?
     return !bit_io(true);
 }
 
 void BitBang_I2C::stop()
 {
-    //PO_DEBUG("");
+    if (verbose) PO_DEBUG("");
     sda->set(0);
     wait();
     scl->set(1);
     wait();
     sda->set(1);
+    wait();
 }
 
 uint8_t BitBang_I2C::io(uint8_t wr, bool *_ack)
@@ -67,9 +68,9 @@ uint8_t BitBang_I2C::io(uint8_t wr, bool *_ack)
     for (int i = 0; i < 8; i++)
     {
         bool d = bit_io(!!(wr & 0x80));
-        wr <<= 1;
-        rd <<= 1;
-        rd += d ? 1 : 0;
+        wr = (uint8_t) (wr << 1);
+        rd = (uint8_t) (rd << 1);
+        if (d) rd |= 1;
     }
     bool a = ack();
     if (_ack) *_ack = a;
@@ -115,28 +116,35 @@ bool BitBang_I2C::io_write_read(uint8_t cmd, const uint8_t *wr, int wr_len, uint
 
 uint8_t BitBang_I2C::make_cmd(uint8_t addr, bool wr)
 {
-    return (addr << 1) + (wr ?  0 : 1);
+    return (uint8_t) ((addr << 1) + (wr ?  0 : 1));
 }
 
     /*
      *
      */
 
-BitBang_I2C::BitBang_I2C(Mutex *m, GPIO *_scl, GPIO *_sda, void (*_wait)())
+BitBang_I2C::BitBang_I2C(Mutex *m, GPIO *_scl, GPIO *_sda, void (*_wait)(), bool _verbose)
 :   mutex(m),
     sda(_sda),
     scl(_scl),
-    wait_fn(_wait)
+    wait_fn(_wait),
+    verbose(_verbose)
 {
     ASSERT(sda);
     ASSERT(scl);
 }
 
+BitBang_I2C::~BitBang_I2C()
+{
+}
+
 bool BitBang_I2C::probe(uint8_t addr, uint32_t timeout)
 {
+    PO_DEBUG("addr=%#x", addr);
+    IGNORE(timeout);
     Lock lock(mutex);
 
-    const bool ok = io_write_read(make_cmd(addr, true), 0, 0, 0, 0);
+    const bool ok = io_write_read(make_cmd(addr, false), 0, 0, 0, 0);
     //PO_DEBUG("addr=%#x ack=%d", addr, ok);
     return ok;
 }
@@ -154,6 +162,11 @@ int BitBang_I2C::write_read(uint8_t addr, const uint8_t* wr, uint32_t len_wr, ui
     Lock lock(mutex);
 
     PO_ERROR("TODO");
+    IGNORE(addr);
+    IGNORE(wr);
+    IGNORE(len_wr);
+    IGNORE(rd);
+    IGNORE(len_rd);
     ASSERT(0);
     return 0;
 }
@@ -163,6 +176,9 @@ int BitBang_I2C::read(uint8_t addr, uint8_t* rd, uint32_t len)
     Lock lock(mutex);
 
     PO_ERROR("TODO");
+    IGNORE(addr);
+    IGNORE(rd);
+    IGNORE(len);
     ASSERT(0);
     return 0;
 }
