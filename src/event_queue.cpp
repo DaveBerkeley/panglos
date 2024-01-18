508e4b467e19edf3c5f3c7e584a2d903b58c7c16
Dave Berkeley <aea@rotwang.co.uk>
2024-01-18 18:15:59 +0000

    make EvQueue into templated class : so I can use 64-bit as well as 32-bit lists

--- src/event_queue.cpp
+++ src/event_queue.cpp
@@ -1,98 +1,19 @@
 
-#include <gtest/gtest.h>
+#include <stdint.h>
 
-#include <panglos/debug.h>
+//#include <panglos/debug.h>
 
-#include <panglos/mutex.h>
 #include <panglos/time.h>
-
 #include <panglos/event_queue.h>
 
 namespace panglos {
 
-static int cmp_t(Time::tick_t t1, Time::tick_t t2)
+template<>
+int _EvQueue<Time::tick_t>::Event::cmp_t(Time::tick_t t1, Time::tick_t t2)
 {
     const int diff = int(t2 - t1);
     //PO_DEBUG("%#x %#x %d", t1, t2, diff);
     return diff;
-}
-
-int EvQueue::Event::cmp(Event *ev1, Event* ev2)
-{
-    return cmp_t(ev1->when, ev2->when);
-}
-
-    /*
-     *
-     */
-
-EvQueue::EvQueue()
-:   mutex(0),
-    events(Event::next_fn)
-{
-    mutex = panglos::Mutex::create();
-}
-
-EvQueue::~EvQueue()
-{
-    delete mutex;
-}
-
-void EvQueue::add(EvQueue::Event *ev)
-{
-    //PO_DEBUG("%p %#x", ev, ev->when);
-    events.add_sorted(ev, Event::cmp, mutex);
-}
-
-bool EvQueue::del(EvQueue::Event *ev)
-{
-    //PO_DEBUG("%p", ev);
-    return events.remove(ev, mutex);
-}
-
-void EvQueue::reschedule(EvQueue::Event *ev, Time::tick_t t)
-{
-    //PO_DEBUG("%p %d", ev, t);
-    del(ev);
-    ev->when = t;
-    add(ev);
-}
-
-EvQueue::Event *EvQueue::pop(Time::tick_t t)
-{
-    panglos::Lock lock(mutex);
-    EvQueue::Event *ev = events.head;
-
-    if (!ev)
-    {
-        return 0;
-    }
-
-    int diff = cmp_t(ev->when, t);
-    //PO_DEBUG("diff=%d", diff);
-
-    if (diff < 0)
-    {
-        return 0;
-    }
-
-    return events.pop(0);
-}
-
-bool EvQueue::run(Time::tick_t t)
-{
-    bool found = false;
-
-    while (true)
-    {
-        EvQueue::Event *ev = pop(t);
-        if (!ev)
-        {
-            return found;
-        }
-        ev->run(this);
-        found = true;
-    }
 }
 
 }   //  namespace panglos
--- src/panglos/event_queue.h
+++ src/panglos/event_queue.h
@@ -8,37 +8,98 @@
 
 namespace panglos {
 
-class EvQueue
+template <class T>
+class _EvQueue
 {
     Mutex *mutex;
 public:
     class Event {
     public:
         Event *next;
-        Time::tick_t when;
+        T when;
 
-        Event(Time::tick_t t=0) : next(0), when(t) { }
+        Event(T t=0) : next(0), when(t) { }
         virtual ~Event() {}
 
-        virtual void run(EvQueue *) = 0;
+        virtual void run(_EvQueue *) = 0;
 
         static Event **next_fn(Event *ev) { return & ev->next; }
-        static int cmp(Event *ev1, Event* ev2);
+        static int cmp_t(T t1, T t2);
+        static int cmp(Event *ev1, Event* ev2)
+        {
+            return cmp_t(ev1->when, ev2->when);
+        }
     };
 
     List<Event*> events;
 
-    EvQueue();
-    ~EvQueue();
+    _EvQueue()
+    :   mutex(0),
+        events(Event::next_fn)
+    {
+        mutex = panglos::Mutex::create();
+    }
 
-    void add(EvQueue::Event *ev);
-    bool del(EvQueue::Event *ev);
-    void reschedule(EvQueue::Event *ev, Time::tick_t t);
+    ~_EvQueue()
+    {
+        delete mutex;
+    }
 
-    EvQueue::Event *pop(Time::tick_t t);
+    void add(_EvQueue::Event *ev)
+    {
+        events.add_sorted(ev, Event::cmp, mutex);
+    }
 
-    bool run(Time::tick_t t);
+    bool del(_EvQueue::Event *ev)
+    {
+        return events.remove(ev, mutex);
+    }
+
+    void reschedule(_EvQueue::Event *ev, T t)
+    {
+        del(ev);
+        ev->when = t;
+        add(ev);
+    }
+
+    Event *pop(T t)
+    {
+        panglos::Lock lock(mutex);
+        _EvQueue::Event *ev = events.head;
+
+        if (!ev)
+        {
+            return 0;
+        }
+
+        int diff = Event::cmp_t(ev->when, t);
+
+        if (diff < 0)
+        {
+            return 0;
+        }
+
+        return events.pop(0);
+    }
+
+    bool run(T t)
+    {
+        bool found = false;
+
+        while (true)
+        {
+            _EvQueue::Event *ev = pop(t);
+            if (!ev)
+            {
+                return found;
+            }
+            ev->run(this);
+            found = true;
+        }
+    }
 };
+
+typedef _EvQueue<Time::tick_t> EvQueue;
 
 }   //  namespace panglos
 
