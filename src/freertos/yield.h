bd6d53e58ff1c48e594c70608eeed79eb54d5f4d
Dave Berkeley <dave@rotwang.co.uk>
2026-06-27 11:51:36 +0100

    task yield for stm32 FreeRTOS build

--- src/freertos/yield.h
+++ src/freertos/yield.h
@@ -3,6 +3,8 @@ static inline void yield_from_isr()
 {
 #if defined(ESP32)
     portYIELD_FROM_ISR();
+#elif defined(STM32)
+    portYIELD_FROM_ISR(0);
 #else
 #error "Different FreeRTOS Port needed"
 #endif
