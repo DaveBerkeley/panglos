65e1a0397e22ac68b22baea587eb4dbc33855492
Dave Berkeley <dave@rotwang.co.uk>
2026-06-29 13:04:13 +0100

    All new gtest simulation for embedded targets

--- gtest.cpp
+++ gtest.cpp
@@ -1,6 +1,112 @@
 
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
+#include "panglos/debug.h"
+#include "panglos/logger.h"
+#include "panglos/time.h"
 
+static Test *tests = 0;
+
+Test::Test(const char *_group, const char *_name, void (*_fn)())
+:   group(_group),
+    name(_name),
+    fn(_fn),
+    next(0)
+{
+    // Link this Test into the global list
+    next = tests;
+    tests = this;
+}
+
+struct Idle
+{
+    void (*fn)(void*);
+    void *arg;
+};
+
+void Test::visit(const char *group, const char *name, visitor fn, void *arg)
+{
+    struct Idle *idle = (struct Idle *) arg;
+    if (idle) ASSERT(idle->fn);
+
+    ASSERT(fn);
+    for (Test *test = tests; test; test = test->next)
+    {
+        if (group && strcmp(group, test->group))
+        {
+            // group set but no match
+            continue;
+        }
+        if (name && strcmp(name, test->name))
+        {
+            // name set but no match
+            continue;
+        }
+        fn(test, 0);
+
+        if (idle) idle->fn(idle->arg);
+    }
+}
+
+void v_run(Test *test, void *arg)
+{
+    UNUSED(arg);
+    PO_DEBUG("%s", test->name);
+ 
+    static const char * disabledPrefix = "DISABLED_";
+
+    if (strncmp(disabledPrefix, test->name, strlen(disabledPrefix)) == 0)
+    {
+        PO_INFO("test %s.%s DISABLED", test->group, test->name);
+    }
+    else
+    {
+        PO_INFO("run test %s.%s", test->group, test->name);
+        test->fn();
+        PO_INFO("run test %s.%s OKAY", test->group, test->name);
+    }
+}
+
+void Test::run(const char *group, const char *name, void (*fn)(void*), void *arg)
+{
+    PO_INFO("Running tests ...");
+    struct Idle idle = { .fn = fn, .arg = arg };
+    visit(group, name, v_run, fn ? & idle : 0);
+    PO_INFO("Tests completed");
+}
+
+    /*
+     *
+     */
+
+extern "C" void test_run(const char *group, const char *name)
+{
+    Test::run(group, name);
+}
+
+    /*
+     *
+     */
+
+struct v_list_t {
+    void *arg;
+    test_visitor fn;
+};
+
+void v_list(Test *test, void *arg)
+{
+    ASSERT(test);
+    ASSERT(arg);
+    struct v_list_t *info = (struct v_list_t *) arg;
+
+    info->fn(test->group, test->name, info->arg);
+}
+
+extern "C" void test_list(const char *group, const char *name, test_visitor fn, void *arg)
+{
+    ASSERT(fn);
+    struct v_list_t info = { .arg = arg, .fn = fn };
+    Test::visit(group, name, v_list, & info);
+}
 
 //  FIN
--- gtest/gtest.h
+++ gtest/gtest.h
@@ -1,66 +1,66 @@
 
-#if !defined(__PANGLOS_GTEST__)
-#define __PANGLOS_GTEST__
+#if defined(__cplusplus)
+
+    /*
+     *
+     */
 
 #include <string.h>
 
 #include "panglos/debug.h"
+#include "panglos/logger.h"
+
+class Test
+{
+    const char *group;
+    const char *name;
+    void (*fn)();
+    Test *next;
+
+    typedef void (*visitor)(Test *, void *arg);
+ 
+    friend void v_run(Test *test,  void *arg);
+    friend void v_list(Test *test,  void *arg);
+ 
+public:
+    Test(const char *_group, const char *_name, void (*_fn)());
+
+    static void run(const char *group, const char *name, void (*idle)(void *)=0, void *arg=0);
+    static void visit(const char *group, const char *name, visitor fn, void *arg);
+    static void add(Test *test);
+};
+
+#define __STRINGISE(x)  #x
+#define STRINGISE(x)    __STRINGISE(x)
+
+#define EXPECT_EQ(a, b)     ASSERT_PRINT((a) == (b), "Failed EXPECT_EQ %s, %s", STRINGISE(a), STRINGISE(b))
+#define EXPECT_NE(a, b)     ASSERT_PRINT((a) != (b), "Failed EXPECT_NE %s, %s", STRINGISE(a), STRINGISE(b))
+#define EXPECT_GT(a, b)     ASSERT_PRINT((a) > (b),  "Failed EXPECT_GT %s, %s", STRINGISE(a), STRINGISE(b))
+#define EXPECT_GE(a, b)     ASSERT_PRINT((a) >= (b), "Failed EXPECT_GE %s, %s", STRINGISE(a), STRINGISE(b))
+#define EXPECT_LT(a, b)     ASSERT_PRINT((a) < (b),  "Failed EXPECT_LT %s, %s", STRINGISE(a), STRINGISE(b))
+#define EXPECT_LE(a, b)     ASSERT_PRINT((a) <= (b), "Failed EXPECT_LE %s, %s", STRINGISE(a), STRINGISE(b))
+#define EXPECT_TRUE(a)      ASSERT_PRINT((a),        "Failed EXPECT_TRUE %s",   STRINGISE(a))
+#define EXPECT_FALSE(a)     ASSERT_PRINT(!(a),       "Failed EXPECT_FALSE %s",  STRINGISE(a))
+#define EXPECT_STREQ(a, b)  ASSERT_PRINT(!strcmp((a), (b)), "Failed EXPECT_STREQ %s, %s", STRINGISE(a), STRINGISE(b))
+
+#define TEST(group,fn) \
+    extern void test_ ##group ##fn(); \
+    static Test group ## fn ##_xx(STRINGISE(group), STRINGISE(fn), test_ ##group ##fn); \
+    void test_ ##group ##fn()
+
+#endif  //  __cplusplus
 
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
-    /*
-     *
-     */
+typedef void (*test_visitor)(const char *group, const char *name, void *arg);
 
-struct TestDef {
-    void (*fn)();
-    const char *group;
-    const char *name;
-    struct TestDef *next;
-};
-
-extern struct TestDef *test_head;
-
-#define TEST(group,name) \
-        void fn_ ## group ## name(); \
-        struct TestDef def_ ## group ## name = { \
-            fn_ ## group ## name, \
-            #group, \
-            #name, \
-            0, \
-        }; \
-        __attribute__((constructor)) static void add_ ## group ## name() \
-        {  \
-            def_ ## group ## name .next = test_head, \
-            test_head = & def_ ## group ## name; \
-        } \
-        void fn_ ## group ## name()
-
-    /*
-     *
-     */
-
-void test_list();
-void test_fail(const char *file, int line, const char *err);
-void test_run();
-
-    /*
-     *
-     */
-
-#define EXPECT_TRUE(a)      if (!(a)) { test_fail(__FILE__, __LINE__, #a); }
-#define EXPECT_FALSE(a)     if ((a)) { test_fail(__FILE__, __LINE__, #a); }
-
-#define EXPECT_EQ(a,b) if ((a) != (b)) { test_fail(__FILE__, __LINE__, #a "!=" #b); }
-
-#define EXPECT_STREQ(a, b) if (strcmp((a), (b))) { test_fail(__FILE__, __LINE__, #a "!=" #b); }
+void test_list(const char *group, const char *name, test_visitor fn, void *arg);
+void test_run(const char *group, const char *name);
 
 #if defined(__cplusplus)
 }
 #endif
-
-#endif  //  __PANGLOS_GTEST__
 
 //  FIN
